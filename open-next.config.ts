74a885bccf846c775663c9b9f6649ff612cc2ecc
Sky Li <skyli@skyli.xyz>
2025-12-26 16:24:05 +0800

    chore: enable minification in OpenNext config

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,5 +2,9 @@ import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 export default defineCloudflareConfig({
   // incrementalCache: r2IncrementalCache,
+  build: {
+    minify: true,
+  },
 });
+
 
